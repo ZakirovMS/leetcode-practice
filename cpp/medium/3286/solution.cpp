3671f300eceb05d91baf17a71b18a49be590332e
ZakirovMS <mz.08.08.06@gmail.com>
2026-07-09 19:47:07 +0300

    Some tests

--- cpp/medium/3286/solution.cpp
+++ cpp/medium/3286/solution.cpp
@@ -9,22 +9,22 @@ std::list< std::pair< int, int > > & p3286::Solution::expandWave(std::vector< st
   {
     int m = i->first;
     int n = i->second;
-    if (m != 0 && weightMtx[m - 1][n] == -1)
+    if (m != 0 && (weightMtx[m - 1][n] == -1 || weightMtx[m - 1][n] > weightMtx[m][n] + grid[m - 1][n]))
     {
       newWave.push_back(std::make_pair(m - 1, n));
       weightMtx[m - 1][n] = weightMtx[m][n] + grid[m - 1][n];
     }
-    if (n != 0 && weightMtx[m][n - 1] == -1)
+    if (n != 0 && (weightMtx[m][n - 1] == -1 || weightMtx[m][n - 1] > weightMtx[m][n] + grid[m][n - 1]))
     {
       newWave.push_back(std::make_pair(m, n - 1));
       weightMtx[m][n - 1] = weightMtx[m][n] + grid[m][n - 1];
     }
-    if (m != grid.size() - 1 && weightMtx[m + 1][n] == -1)
+    if (m != grid.size() - 1 && (weightMtx[m + 1][n] == -1 || weightMtx[m + 1][n] > weightMtx[m][n] + grid[m + 1][n]))
     {
       newWave.push_back(std::make_pair(m + 1, n));
       weightMtx[m + 1][n] = weightMtx[m][n] + grid[m + 1][n];
     }
-    if (n != grid[0].size() - 1 && weightMtx[m][n + 1] == -1)
+    if (n != grid[0].size() - 1 && (weightMtx[m][n + 1] == -1 || weightMtx[m][n + 1] > weightMtx[m][n] + grid[m][n + 1]))
     {
       newWave.push_back(std::make_pair(m, n + 1));
       weightMtx[m][n + 1] = weightMtx[m][n] + grid[m][n + 1];
@@ -32,7 +32,7 @@ std::list< std::pair< int, int > > & p3286::Solution::expandWave(std::vector< st
   }
 
   std::swap(wave, newWave);
-  return newWave;
+  return wave;
 }
 
 bool p3286::Solution::findSafeWalk(std::vector< std::vector< int > > & grid, int health)
@@ -47,7 +47,7 @@ bool p3286::Solution::findSafeWalk(std::vector< std::vector< int > > & grid, int
     std::list< std::pair< int, int > > newWave = expandWave(grid, wave, weightMtx);
   }
 
-  if (weightMtx[grid.size() - 1][grid[0].size() - 1] > health)
+  if (weightMtx[grid.size() - 1][grid[0].size() - 1] >= health)
   {
     return false;
   }
--- cpp/medium/3286/test-solution.cpp
+++ cpp/medium/3286/test-solution.cpp
@@ -1,1 +1,37 @@
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
+#include "solution.hpp"
+
+BOOST_AUTO_TEST_CASE(regularSquareDataTest)
+{
+  std::vector< std::vector< int > > source = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+  p3286::Solution sol;
+  BOOST_TEST(sol.findSafeWalk(source, 1) == true);
+}
+
+BOOST_AUTO_TEST_CASE(failSquareDataTest)
+{
+  std::vector< std::vector< int > > source = {{0, 0, 0}, {1, 1, 1}, {0, 0, 0}};
+  p3286::Solution sol;
+  BOOST_TEST(sol.findSafeWalk(source, 1) == false);
+}
+
+BOOST_AUTO_TEST_CASE(regularRectangleDataTest)
+{
+  std::vector< std::vector< int > > source = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+  p3286::Solution sol;
+  BOOST_TEST(sol.findSafeWalk(source, 1) == true);
+}
+
+BOOST_AUTO_TEST_CASE(failRectangleDataTest)
+{
+  std::vector< std::vector< int > > source = {{0, 0, 0}, {1, 1, 1}, {0, 0, 0}, {0, 0, 0}};
+  p3286::Solution sol;
+  BOOST_TEST(sol.findSafeWalk(source, 1) == false);
+}
+
+BOOST_AUTO_TEST_CASE(LabyrinthRectangleDataTest)
+{
+  std::vector< std::vector< int > > source = {{0, 0, 0}, {1, 0, 1}, {0, 0, 0}, {0, 0, 0}};
+  p3286::Solution sol;
+  BOOST_TEST(sol.findSafeWalk(source, 1) == true);
+}
